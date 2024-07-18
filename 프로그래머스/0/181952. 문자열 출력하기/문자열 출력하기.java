6ad2b150440d285aac8a65e7774c0f4b9940eee2
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2024-07-18 10:11:00 +0900

    [level 0] Title: 문자열 출력하기, Time: 260.15 ms, Memory: 63.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        System.out.println(str);
+    }
+}
