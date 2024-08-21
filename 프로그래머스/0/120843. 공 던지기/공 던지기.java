7d872decafb349558d92039fbe28fcda432f4ebe
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2024-08-21 11:54:09 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,27 +1,5 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 1;
-        if(numbers.length % 2 == 0){
-            for(int i = 1; i<k; i++){
-                answer += 2;
-                if(answer >= numbers.length){
-                    answer = 1;
-                }
-            }
-        }else{
-            for(int i = 1; i<k; i++){
-                answer += 2;
-                System.out.println("현재 값: " + answer);
-                if(answer > numbers.length){
-                    System.out.println("현재 값은: " + answer+"이고, if문 안에 들어옴");
-                    if(answer % 2 == 1){
-                        answer = 2;
-                    }else{
-                        answer = 1;
-                    }
-                }
-            }
-        }
-        return answer;
+        return (k-1)*2 % numbers.length+1;
     }
 }
