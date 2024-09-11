50eb56654168152d18d3a0e205c67d26a13a8732
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2024-09-11 14:50:55 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 4.23 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String word = "";
+        List<String> sta = new ArrayList<String>();
+        my_string = my_string.replaceAll("[A-Z,a-z]"," ");
+        for(int i = 0; i<my_string.length(); i++){
+            sta.add(Character.toString(my_string.charAt(i)));
+            System.out.println(sta.get(i));
+        }
+        sta.add(" ");
+        for(int  i = 0; i<sta.size(); i++){
+            String num = sta.get(i);
+            if(!num.equals(" ")){
+                word += num;
+            }else if(!word.equals("")){
+                System.out.println("지금 들어옴 " + word);
+                answer += Integer.parseInt(word);
+                word = "";
+            }
+        }
+        return answer;
+    }
+}
