0ba1e6ee33644b1b9debb0e05a54ac4eb493d285
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2024-09-16 19:57:03 +0900

    [level 0] Title: 저주의 숫자 3, Time: 14.26 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,12 +1,13 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
- 
-        for (int i = 0; i < n; i++) {      
-            answer++;                        
-            while (answer % 3 == 0 || String.valueOf(answer).contains("3")) { 
-                answer++;                 
+        System.out.println(answer);
+        for(int i = 0; i<n; i++){
+            answer++;
+            while(answer % 3 == 0 || String.valueOf(answer).contains("3")){
+                answer++;
             }
+            System.out.println("현재 i: " + i+ " answer: " + answer);
         }
         return answer;
     }
