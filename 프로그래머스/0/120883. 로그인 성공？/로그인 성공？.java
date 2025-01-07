c851c02dc79fefbf8542fa7269934e145e1feff4
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2025-01-07 16:43:06 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        
+        for(int i = 0; i<db.length; i++){
+            if(id_pw[0].equals(db[i][0])){
+                if(id_pw[1].equals(db[i][1])){
+                    
+                    return "login";
+                }else{
+                    return "wrong pw";
+                }
+            }else{
+                answer = "fail";
+            }
+        }
+        return answer;
+    }
+}
