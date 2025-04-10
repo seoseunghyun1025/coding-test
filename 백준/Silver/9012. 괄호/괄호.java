ad8fad61ab854396e46028ef2adfe6b275fce8d9
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2025-04-10 20:35:10 +0900

    [Silver IV] Title: 괄호, Time: 200 ms, Memory: 18020 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		List<String> list = new ArrayList<>();
+		Main m = new Main();
+		int x = Integer.parseInt(in.nextLine());
+		
+		for(int i = 0; i<x; i++) {
+			String a = in.nextLine();
+			list.add(m.isVPS(a));
+		}
+		for(String s : list) {
+			System.out.println(s);
+		}
+		
+		in.close();
+	}
+	private String isVPS(String s) {
+		Stack<Character> sta = new Stack<>();
+		char c[] = s.toCharArray();
+		
+		for(int i = 0; i<c.length; i++) {
+			if(c[i] == '(') {
+				sta.push(c[i]);
+			}
+			if(c[i] == ')') {
+				if(sta.isEmpty()) {
+					return "NO";
+				}
+				sta.pop();
+			}
+		}
+		if(sta.isEmpty()) {
+			return "YES";
+		}
+		return "NO";
+	}
+}
