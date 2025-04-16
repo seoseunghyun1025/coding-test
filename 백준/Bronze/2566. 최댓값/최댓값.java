ab9c8d379db83a569062677ec29a249f8076b428
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2025-04-16 17:56:27 +0900

    [Bronze III] Title: 최댓값, Time: 192 ms, Memory: 18492 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -5,7 +5,7 @@ class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int[][] arr = new int[9][9];
-        int max = 0, i = 0, j = 0, maxI = 0, maxJ = 0;
+        int max = -1, i = 0, j = 0, maxI = 0, maxJ = 0;
         for(i = 0; i < arr.length; i++){
             for(j = 0; j < arr[i].length; j++){
                 arr[i][j] = in.nextInt();
@@ -15,12 +15,12 @@ class Main {
             for(j = 0; j < arr[i].length; j++){
                 if(max < arr[i][j]){
                     max = arr[i][j];
-                    maxI = i;
-                    maxJ = j;
+                    maxI = i + 1;
+                    maxJ = j + 1;
                 }
             }
         }
         System.out.println(max);
-        System.out.println((maxI + 1) + " " + (maxJ + 1));
+        System.out.println(maxI + " " + maxJ);
     }
 }
