a7983e9e86480f74916e4c8981f9b96b207faa21
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2025-04-24 23:36:03 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 172 ms, Memory: 17776 KB, Score: 46 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -4,19 +4,20 @@ public class Main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int pre = 1, prepre = 1, answer = 0;
-        if(n == 1){
+        long pre = 1, prepre = 1;
+        long answer = 0;
+        if(n == 0){
             System.out.println(0);
+            return ;
+        }else if(n == 1){
+            System.out.println(pre);
             return ;
         }else if(n == 2){
             System.out.println(pre);
             return ;
-        }else if(n == 3){
-            System.out.println(pre);
-            return ;
         }
 
-        for(int i = 4; i <= n; i++){
+        for(int i = 3; i <= n; i++){
             answer = pre + prepre;
             prepre = pre;
             pre = answer;
