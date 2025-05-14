2f1d7fe37484f4314b5ad6ca3ba0ff4b36a4623e
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2025-05-14 15:21:35 +0900

    [Silver IV] Title: 차집합, Time: 4656 ms, Memory: 390104 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.java
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -3,23 +3,39 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-        int n = in.nextInt(), m = in.nextInt();
-
-        for(int i=0; i < n; i++){
-            set1.add(in.nextInt());
+        // 두 집합의 원소 개수를 입력받는다.
+        int n = in.nextInt();
+        int m = in.nextInt();
+        
+        // 집합 A와 B의 원소들을 저장할 Set 선언
+        Set<Integer> setA = new HashSet<>();
+        Set<Integer> setB = new HashSet<>();
+        
+        // 집합 A의 원소들을 입력받아 setA에 저장
+        for (int i = 0; i < n; i++) {
+            setA.add(in.nextInt());
         }
-        for(int i=0; i < m; i++){
-            set2.add(in.nextInt());
+        
+        // 집합 B의 원소들을 입력받아 setB에 저장
+        for (int i = 0; i < m; i++) {
+            setB.add(in.nextInt());
         }
-        set1.removeAll(set2);
-        System.out.println(set1.size());
-        List<Integer> list = new ArrayList<>(set1);
-        Collections.sort(list);
-        for(int i : list){
-            System.out.print(i + " ");
+        
+        // 집합 A에서 집합 B에 속하지 않는 원소들만 추출
+        setA.removeAll(setB);
+        
+        // 집합 A에서 집합 B에 속하지 않는 원소들이 없다면 0을 출력
+        System.out.println(setA.size());
+        
+        // 존재하면 원소들을 오름차순으로 출력
+        if (setA.size() > 0) {
+            List<Integer> result = new ArrayList<>(setA);
+            Collections.sort(result);
+            for (int num : result) {
+                System.out.print(num + " ");
+            }
         }
+        
         in.close();
     }
 }
