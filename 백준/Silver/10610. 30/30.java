4e74e26c04bcae4e5eeb3ed78921584ed945ea1e
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2025-05-15 15:18:34 +0900

    [Silver IV] Title: 30, Time: 160 ms, Memory: 16572 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        char[] arr = input.toCharArray();
+        Arrays.sort(arr);
+
+        int sum = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int num = arr[i] - '0';
+            sum += num;
+            sb.append(num);
+        }
+
+        if(sum % 3 != 0 || arr[0] != '0') {
+            System.out.println(-1);
+            return;
+        }
+
+        System.out.println(sb);
+    }
+}
