042595cb71c8d429cebbd77e51ab3da78903e703
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2025-05-19 09:50:57 +0900

    [Silver II] Title: A → B, Time: 116 ms, Memory: 17560 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,37 +1,31 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    static long a,b;
-    static int cnt;
-
-    static int bfs(){
-        Queue<Long> q = new LinkedList<>();
-        q.add(a);
-
-        while(!q.isEmpty()){
-            int size = q.size();
-
-            for(int i=0; i<size; i++){
-                long tmp = q.poll();
-                if(tmp==b)
-                    return cnt+1;
-
-                if(tmp*2<=b) q.add(tmp*2);
-                if(tmp*10+1<=b) q.add(tmp*10+1);
-            }
-            cnt++;
-        }
-        return -1;
-    }
-
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
+        Queue<Long> que = new LinkedList<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-
-        a = Long.parseLong(s[0]);
-        b = Long.parseLong(s[1]);
-
-        System.out.println(bfs());
+        String[] input = br.readLine().split(" ");
+        br.close();
+        long a = Long.parseLong(input[0]), b = Long.parseLong(input[1]);
+        int count = 0;
+        que.add(a);
+        while(!que.isEmpty()){
+            int size = que.size();
+            for(int i = 0; i<size; i++){
+                long tmp = que.poll();
+                if(tmp == b) {
+                    count++;
+                    System.out.println(count);
+                    return;
+                }
+                if(tmp * 2 <= b) que.add(tmp * 2);
+                if(tmp * 10 + 1 <= b) que.add(tmp * 10 + 1);
+            }
+            count++;
+        }
+        System.out.println(-1);
     }
 }
