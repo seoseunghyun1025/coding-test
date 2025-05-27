ee53d86ded82714911999beb78508868a5a05d37
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2025-05-27 23:39:13 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main {
+    static int B[][];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(s+"??!");
+        br.close();
+    }
+}
