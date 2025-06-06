7e3004512be71328124810ab12da5ecee7f98366
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2025-06-06 23:23:04 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static char[][] star;
+    static int count;
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|");
+        System.out.print(sb);
+    }
+}
