ced8bc540191babfa475f3dc145c6a49ad5d1c5f
seoseunghyun1025 <118885745+seoseunghyun1025@users.noreply.github.com>
2025-06-22 22:40:09 +0900

    [Silver III] Title: N과 M (3), Time: 592 ms, Memory: 211880 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    static int[] count;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+        M = m;
+        count = new int[m + 2];
+        func(n, m);
+        System.out.println(sb);
+    }
+    //n이 출력, m이 for문 개수
+    static void func(int n, int m){
+        if(m == 0){
+            for(int i = M; i > 0; i--){
+                sb.append(count[i]+ " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i = 1; i <= n; i++){
+            count[m] = i;
+            func(n, m - 1);
+        }
+    }
+}
